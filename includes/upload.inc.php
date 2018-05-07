7094a639f21d71a13d54cbe6a34678281407cb2a
Kamikali <addi98new@gmail.com>
2018-05-07 09:18:55 +0200

    excluded < and > from title and message in posts

--- includes/upload.inc.php
+++ includes/upload.inc.php
@@ -36,46 +36,52 @@ if(isset($_POST['submit'])){
 			if($fileSize > 8000000){ //MB
 				echo "Your file was too big";
 			} else {
+				if (strpos($_POST["title"], '<') !== false || strpos($_POST["title"], '>') !== false) {
+  					  echo "Title and/or Description are not allowed to include < or >!";
+  				} else {
+					if (strpos($_POST["message"], '<') !== false || strpos($_POST["message"], '>') !== false) {
+	  					  echo "Title and/or Description are not allowed to include < or >!";
+	  				} else {
 
+						$fileNameNew = uniqid('', true).".".$fileActualExt;
+						
 
+						$year = date("Y");
+						$month = date("m");
+						$day = date("d");
+						$directory = "$year/$month/$day/";
+		 
+						if(!is_dir('../uploads/'.$directory)){
+		   					mkdir('../uploads/'.$directory, 755, true);
+						}
 
-				$fileNameNew = uniqid('', true).".".$fileActualExt;
-				
+						//test if upload is a video or a picture
+						$isPic = true;
+						if (($fileActualExt == 'mp4') || ($fileActualExt == 'm4v')) {
+							$isPic = false;
+						} else {
+							$thumbnailName = uniqid('', true).".jpeg";
+						}				
 
-				$year = date("Y");
-				$month = date("m");
-				$day = date("d");
-				$directory = "$year/$month/$day/";
- 
-				if(!is_dir('../uploads/'.$directory)){
-   					mkdir('../uploads/'.$directory, 755, true);
+						
+						
+						//upload file
+						$fileDestination = 'uploads/'.$directory.$fileNameNew;
+						move_uploaded_file($fileTmpName, '../'.$fileDestination);
+						//create thumbnail
+						$thumbnailDirection = 'uploads/'.$directory.$thumbnailName;
+						create_thumbnail('../'.$fileDestination, '../'.$thumbnailDirection, 60, 60);
+						//insert file data into database
+						$upload_user_id = $_SESSION['u_id'];
+						$upload_user_uid = $_SESSION['u_uid'];
+						$country = $_SESSION['u_countryid'];
+						$sql = "INSERT INTO posts(title, message, path, thumbnail_path, user_id, user_uid, country, nsfwl, date, upvotes, downvotes, ispic)    
+								VALUES ('$title', '$message', '$fileDestination', '$thumbnailDirection', '$upload_user_id', '$upload_user_uid', '$country', 'sfw', NOW(), 0, 0, '$isPic')";
+						mysqli_query($conn, $sql);
+						//head back to main page
+						header("Location: ../news.php?=uploadsuccess");
+					}
 				}
-
-				//test if upload is a video or a picture
-				$isPic = true;
-				if (($fileActualExt == 'mp4') || ($fileActualExt == 'm4v')) {
-					$isPic = false;
-				} else {
-					$thumbnailName = uniqid('', true).".jpeg";
-				}				
-
-				
-				
-				//upload file
-				$fileDestination = 'uploads/'.$directory.$fileNameNew;
-				move_uploaded_file($fileTmpName, '../'.$fileDestination);
-				//create thumbnail
-				$thumbnailDirection = 'uploads/'.$directory.$thumbnailName;
-				create_thumbnail('../'.$fileDestination, '../'.$thumbnailDirection, 60, 60);
-				//insert file data into database
-				$upload_user_id = $_SESSION['u_id'];
-				$upload_user_uid = $_SESSION['u_uid'];
-				$country = $_SESSION['u_countryid'];
-				$sql = "INSERT INTO posts(title, message, path, thumbnail_path, user_id, user_uid, country, nsfwl, date, upvotes, downvotes, ispic)    
-						VALUES ('$title', '$message', '$fileDestination', '$thumbnailDirection', '$upload_user_id', '$upload_user_uid', '$country', 'sfw', NOW(), 0, 0, '$isPic')";
-				mysqli_query($conn, $sql);
-				//head back to main page
-				header("Location: ../news.php?=uploadsuccess");
 			}
 		}	
 	}
